a441e9c3d3a424f7de4abf868befd9f1bfcbf916
Louis Augey <louis.augey@orange.fr>
2024-11-13 11:20:23 +0100

    pokemoncard class

--- PokemonCard.h
+++ PokemonCard.h
@@ -4,16 +4,22 @@
 #include <tuple>
 
 class PokemonCard : public Card{
-    public:
+    protected:
         std::string pokemonType;
         std::string familyName;
         int evolutionLevel;
-        int hp;
+        int hp; //if hp is <= 0, pokemon died and is removed from the game
         int maxHP;
         std::vector<std::tuple<int, int, std::string, int>> attacks;
-        int energyCost;
-        int currentEnergyCost;
-        std::string attackDescription;
-        int attackDamage;
-        
+
+    public:
+        PokemonCard(
+            std::string& name, 
+            std::string& type, 
+            std::string& family, 
+            int level, 
+            int health, 
+            int maxHealth, 
+            std::vector<std::tuple<int, int, std::string, int>>& attackList
+        ); 
 };
